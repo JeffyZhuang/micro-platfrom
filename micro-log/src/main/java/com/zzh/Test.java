b31a907cd009ddd5122654751e5329a6cded526f
JeffyZhuang <270225738@qq.com>
2019-08-26 20:36:42 +0800

    log4j commit

---
+++ micro-log/src/main/java/com/zzh/Test.java
@@ -0,0 +1,15 @@
+package com.zzh;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: zzh
+ * @Description:
+ * @Date: 2019/8/26
+ */
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        log.info("Here is a log for info");
+    }
+}
